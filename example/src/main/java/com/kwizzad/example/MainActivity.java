3babd039b19bf64355a01f86415ad75f67317a3a
gzach <gaylord@tvsmiles.de>
2016-08-10 14:37:31 +0200

    Example app for KWIZZAD SDK v.0.3.0

--- example/src/main/java/com/kwizzad/example/MainActivity.java
+++ example/src/main/java/com/kwizzad/example/MainActivity.java
@@ -11,7 +11,8 @@ import android.view.View;
 
 import com.kwizzad.Kwizzad;
 import com.kwizzad.log.QLog;
-import com.kwizzad.model.PendingCallback;
+import com.kwizzad.model.PendingEvent;
+import com.kwizzad.model.events.Reward;
 import com.kwizzad.property.RxSubscriber;
 
 import java.util.HashSet;
@@ -19,10 +20,11 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Set<PendingCallback> shownCallbacks = new HashSet<>();
+    private Set<PendingEvent> shownEvents = new HashSet<>();
     private TextInputLayout placementIdInput;
     private View preloadButton;
     private View simpleButton;
+    private View simplePreloadButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +58,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        simplePreloadButton = findViewById(R.id.simplePreload);
+        if(simplePreloadButton != null) {
+            simplePreloadButton.setOnClickListener(v -> {
+                if (Kwizzad.initialized().get()) {
+                    startActivity(SimplePreloadingExample.createIntent(this, placementIdInput.getEditText().getText().toString()));
+                } else {
+                    Log.e("KWIZZAD", "not initialized");
+                    Snackbar.make(findViewById(R.id.coordinatorLayout), "kwizzad not initialized", Snackbar.LENGTH_INDEFINITE).show();
+                }
+            });
+        }
     }
 
     @Override
@@ -64,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
 
         Kwizzad.resume(this);
 
-        RxSubscriber.subscribe(this, Kwizzad.pendingCallbacks(), pendingCallbacks -> {
-            if (pendingCallbacks.size() > 0) {
-                QLog.d("should show callback " + pendingCallbacks);
+        RxSubscriber.subscribe(this, Kwizzad.pendingEvents(), pendingEvents -> {
+            if (pendingEvents.size() > 0) {
+                QLog.d("should show event " + pendingEvents);
                 // there are multiple callbacks possibly coming. its up to you if you want to show them all now
                 // or you want to show them some very different way.
                 // maybe its something like a notification for you
@@ -74,39 +88,58 @@ public class MainActivity extends AppCompatActivity {
 
                 // we dont wanna show this twice at least for the same session here
                 // you can handle that any way you want though
-                for (PendingCallback pendingCallback : pendingCallbacks) {
-                    if (shownCallbacks.contains(pendingCallback) == false) {
-                        QLog.d("showing callback " + pendingCallback);
-                        showCallback(pendingCallback);
+                for (PendingEvent pendingEvent : pendingEvents) {
+                    if (shownEvents.contains(pendingEvent) == false) {
+                        QLog.d("showing event " + pendingEvent);
+
+                        // How to distinguish between Call2Action (Instant-Reward) and Callback (full billing)
+
+                        if (pendingEvent.type.equals(PendingEvent.Type.CALL2ACTION))
+                        {
+                            // The user has completed the kwizzad and can receive an instant reward (based on guarantee payment if agreed)
+                            // Currently this event does not contain a specific reward amount, so you'll have to set this manually based on contractual agreement with KWIZZAD
+                            final int instantRewardAmount = 5000;
+                            // now you can reward the user
+                        } else if (pendingEvent.type.equals(PendingEvent.Type.CALLBACK))
+                        {
+                            // The advertising partner has confirme the transaction. The user can be rewarded the full amount
+                            Reward reward = pendingEvent.reward;
+                            int rewardAmount = reward.amount;
+                            String rewardCurrency = reward.currency; // e.g. chips, coins, loot, smiles as configued by KWIZZAD.
+                        }
+
+                        showEvent(pendingEvent);
 
                         // just showing the first one
                         return;
                     }
+                    else {
+                        QLog.d("already shown event "+pendingEvent);
+                    }
                 }
-
-                return;
             }
         });
     }
 
-    private void showCallback(final PendingCallback pendingCallback) {
-        shownCallbacks.add(pendingCallback);
+    private void showEvent(final PendingEvent pendingEvent) {
+        shownEvents.add(pendingEvent);
+
 
         new AlertDialog.Builder(this)
-                .setTitle("CALLBACK!")
-                .setMessage(pendingCallback.toString())
+                .setTitle(pendingEvent.type+" !!!")
+                .setMessage(pendingEvent.toString())
                 .setPositiveButton(android.R.string.ok,
                         (dialog, whichButton) -> {
-                            Kwizzad.completeCallback(pendingCallback);
+                            Kwizzad.completeEvent(pendingEvent);
                         }
                 )
                 .setNegativeButton(android.R.string.cancel,
                         (dialog, whichButton) -> {
                             dialog.dismiss();
-                            shownCallbacks.remove(pendingCallback);
+                            shownEvents.remove(pendingEvent);
                         }
                 )
-                .setOnDismissListener(dialog -> shownCallbacks.remove(pendingCallback))
+                .setOnDismissListener(dialog -> shownEvents.remove(pendingEvent))
                 .create()
                 .show();
 
